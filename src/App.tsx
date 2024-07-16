69257db46f9bf4edac292bcc6b7e2cb35e688ae6
Margot Kriete <margot.kriete@gmail.com>
2024-07-16 13:40:24 -0400

    Iterate through images

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import ads from "./directory_names.json";
 import "./App.css";
 
 function App() {
-  console.log(ads);
-
-  const [currentImage, setCurrentImage] = useState(
-    "src/assets/1495631310801_XX-87c86ef41ae28c5a2861a209e10b2dfd21915d28c16ac54274831101d723f164/screensvr.png"
-  );
-  const getNextImage = function () {};
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const getNextImage = function () {
+    setCurrentImageIndex(currentImageIndex + 1);
+  };
 
   return (
     <>
       <button onClick={getNextImage}>Next</button>
-      <img src={currentImage} />
+      <img src={`/src/assets/${ads[currentImageIndex]}/screensvr.png`} />
     </>
   );
 }

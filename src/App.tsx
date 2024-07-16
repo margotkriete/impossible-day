00b69d75ee2d9fbcef955f41774f8e56e3f4cb55
Margot Kriete <margot.kriete@gmail.com>
2024-07-16 13:36:43 -0400

    Put image directory names in JSON file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import ads from "./directory_names.json";
 import "./App.css";
 
 function App() {
-  // var files = fs.readdirSync("/assets/");
+  console.log(ads);
+
   const [currentImage, setCurrentImage] = useState(
     "src/assets/1495631310801_XX-87c86ef41ae28c5a2861a209e10b2dfd21915d28c16ac54274831101d723f164/screensvr.png"
   );
-
   const getNextImage = function () {};
 
   return (

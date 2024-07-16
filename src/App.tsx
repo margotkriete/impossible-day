2f04d24a53bfd47867b119826814e98a0e99fa80
amandayhuang <103937002+a-yh@users.noreply.github.com>
2024-07-16 14:14:02 -0400

    add reveal button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
 import { useState } from "react";
 import ads from "./directory_names.json";
 import "./App.css";
+import { getBookInfo } from "./services/getBookInfo";
+import { OpenLibraryBook } from "./types/Books";
 
 function App() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isDocumentFetched, setIsDocumentFetched] = useState(false);
+  const [document, setDocument] = useState<OpenLibraryBook | null>(null);
   const getNextImage = function () {
+    setIsDocumentFetched(false);
+    setDocument(null);
     setCurrentImageIndex(currentImageIndex + 1);
+  };
+
+  const revealHandler = async () => {
+    const documents = await getBookInfo({
+      title: "lord of the rings",
+      author: "j r r tolkien",
+    });
+    setIsDocumentFetched(true);
+    setDocument(documents.length > 0 ? documents[0] : null);
   };
 
   return (
     <>
       <button onClick={getNextImage}>Next</button>
       <img src={`/src/assets/${ads[currentImageIndex]}/screensvr.png`} />
+      {!isDocumentFetched && <button onClick={revealHandler}>Reveal</button>}
+      {isDocumentFetched && document ? (
+        <>
+          <h4>{document.author_name}</h4>
+          <h4>{document.first_sentence}</h4>
+          <h4>{document.first_publish_year}</h4>
+        </>
+      ) : (
+        <h4>{`hallucinated book found`}</h4>
+      )}
     </>
   );
 }
